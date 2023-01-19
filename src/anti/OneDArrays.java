538c10512510ba3aa9c4fc15e1158281ec6caf73
Ahmed Ashraf <ahmedashraaf09@gmail.com>
2023-01-19 15:47:29 +0200

    Arrays

---
+++ src/anti/OneDArrays.java
@@ -0,0 +1,38 @@
+package anti;
+
+public class OneDArrays { //pascal
+
+
+    public static void main(String[] args) {
+        final  int size = 5 ;
+        int [] arr2 = new int [size]  ;
+         int [] arr = new int [size] ;
+         arr[0]= 1 ;
+         arr[1] = 2 ;
+        arr[2] = 3 ;
+        arr[3] = 4 ;
+        arr[4] = 5 ;
+      /*  arr2 = arr ;
+        arr[0] = 50 ;
+        arr2[3] = 200;
+        System.out.println(arr[3]);*/
+        //Swapping
+        for (int i =0;i<=4 ; i++){
+            arr2[i] = arr [i];
+        }
+        //print
+        for (int i =0;i<=4 ; i++){
+            System.out.println(arr2[i]);
+        }
+
+
+
+
+
+
+        }
+
+    }
+
+
+
---
+++ src/anti/TwoDArrays.java
@@ -0,0 +1,66 @@
+package anti;
+
+import java.util.Scanner;
+
+public class TwoDArrays { //pascal
+
+
+    public static void main(String[] args) {
+/*
+         int [][] arr ={   {1,2,3},
+                            {4,5,6},
+                             {7,8,9}
+                                        };
+       //  String [][] arr2 = new String[6][7];
+        //System.out.println(arr2[0][0]);
+        for (int i =0;i<=2 ; i++){ //row
+            for (int j = 0;j<=2;j++) //columns
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+*/
+      /*  arr2 = arr ;
+        arr[0] = 50 ;
+        arr2[3] = 200;
+        System.out.println(arr[3]);*/
+        //Swapping
+      /*  for (int i =0;i<=4 ; i++){
+            arr2[i] = arr [i];
+        }
+        //print
+        for (int i =0;i<=4 ; i++){
+            System.out.println(arr2[i]);
+        }*/
+
+
+        //Example
+        Scanner input = new Scanner(System.in);
+        int [][] array = new int[4][4] ;
+        int sum = 0;
+        int dSum = 0;
+        for (int i = 0 ; i<= 3 ;i++) //rows
+        {
+            for(int j =0; j<=3;j++) //column
+            {
+                System.out.println("Enter Number" + (i)+(j));
+                array[i][j]= input.nextInt();
+                sum += array[i][j];
+                if(i==j)
+                   dSum += array[i][j] ;
+            }
+        }
+        System.out.println("Sum= " + sum + " Diagonal= " + dSum);
+
+
+
+
+
+        }
+
+    }
+
+
+
