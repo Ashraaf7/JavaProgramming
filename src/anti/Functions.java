7210510ee8b6ca7009165b4904f9f10186535b51
Ahmed Ashraf <ahmedashraaf09@gmail.com>
2023-01-19 13:51:36 +0200

    Functions

---
+++ src/anti/Functions.java
@@ -0,0 +1,44 @@
+package anti;
+
+public class Functions { //pascal
+
+
+    public static void main(String[] args) {
+
+        int x =  5;
+        int y = 10;
+        //calling
+        sum(x,y);
+       int w = 50 ;//global
+       w += 90 ;
+       int ay7aga = CalcSum(10,20,30);
+        System.out.println(ay7aga);
+        System.out.println(CalcAvg2(ay7aga));
+
+    }
+        //define
+    public  static  int sum (int z , int q)
+    {
+        int res = z + q ; //local
+        return  res ;
+        //System.out.println( number1 + number2);
+    }
+
+    public  static  int CalcSum(int n1 , int n2 , int n3)
+    {
+        return n1+ n2 +n3 ;
+    }
+    public  static  int CalcAvg1(int n1 , int n2 , int n3)
+    {
+        return (n1+ n2 +n3)/3 ;
+    }
+    public  static  int CalcAvg2(int ay7aga)
+    {
+        return ay7aga/3 ;
+    }
+
+
+    }
+
+
+
